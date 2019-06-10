2536004b88eac0985e78a8734aabc108740dd3a9
seeduardo <romenflatiron@gmail.com>
2019-06-10 16:48:44 +0100

    Added MedalCountTable component to house everything else

--- olympic-medals-widget/src/App.js
+++ olympic-medals-widget/src/App.js
@@ -1,17 +1,11 @@
 import React from 'react';
-
 import './App.css';
+import MedalCountTable from './MedalCountTable';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src="http://localhost:3000/hope_springs_eternal.png" className="App-logo" alt="logo" />
-        <p>
-          Eventually there will be a sortable medal table here that looks a bit like the one spinning above....
-        </p>
-        
-      </header>
+      <MedalCountTable/>
     </div>
   );
 }
---
+++ olympic-medals-widget/src/MedalCountTable.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+function MedalCountTable() {
+  return (
+    <div className="MedalCountTable">
+      <table>
+        <header>MEDAL COUNT</header>
+        <tbody>
+          <tr>Medal Types will be here</tr>
+          <tr>Data will appear here</tr>
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default MedalCountTable;
