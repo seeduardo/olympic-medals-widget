cdc87e7a2f0182aed237b6c0be5b55af08d900cf
seeduardo <romenflatiron@gmail.com>
2019-06-11 00:16:39 +0100

    Fetched medalData in MedalCountTable and passed to CountryMedalCountContainer

--- olympic-medals-widget/src/MedalCountTable.js
+++ olympic-medals-widget/src/MedalCountTable.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import MedalCountType from './MedalCountType';
 import CountryFlagContainer from './CountryFlagContainer';
 import CountryMedalCountContainer from './CountryMedalCountContainer';
 
 function MedalCountTable() {
+
+  const medalDataUrl = 'https://s3-us-west-2.amazonaws.com/reuters.medals-widget/medals.json'
+  const [medalData, setMedalData] = useState([]);
+
+  useEffect(
+    () => {
+      getMedalData();
+    }, []
+  );
+
+  async function getMedalData() {
+    const response = await fetch(medalDataUrl);
+    const medalData = await response.json();
+    setMedalData(medalData);
+    console.log(medalData);
+  }
+
   return (
     <div className="MedalCountTable">
       <header>MEDAL COUNT</header>
@@ -21,7 +38,7 @@ function MedalCountTable() {
               <CountryFlagContainer/>
             </td>
             <td>
-              <CountryMedalCountContainer/>
+              <CountryMedalCountContainer medalData={medalData}/>
             </td>
           </tr>
         </tbody>
