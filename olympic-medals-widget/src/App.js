5ba5e27708d9a4f2f76dc9f0967ed2f5179301b2
seeduardo <romenflatiron@gmail.com>
2019-06-10 15:57:08 +0100

    Initialized project and added temporary front page

---
+++ olympic-medals-widget/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="http://localhost:3000/hope_springs_eternal.png" className="App-logo" alt="logo" />
+        <p>
+          Eventually there will be a sortable medal table here that looks a bit like the one spinning above....
+        </p>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
