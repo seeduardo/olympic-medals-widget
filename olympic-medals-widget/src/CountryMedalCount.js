24472e386e60a2dbd0415bb349924d16d4dfe30e
seeduardo <romenflatiron@gmail.com>
2019-06-11 16:01:52 +0100

    Split flags with ImageMagick and started adding them to application

--- olympic-medals-widget/src/CountryMedalCount.js
+++ olympic-medals-widget/src/CountryMedalCount.js
@@ -2,9 +2,13 @@ import React from 'react';
 import './App.css';
 
 function CountryMedalCount(props) {
+
+  const flagTilesUrl = 'http://localhost:3000/flags/flagtiles/'
+
   return (
     <div className="CountryMedalCount">
       <ul>
+        <li><img src={flagTilesUrl + "flag_tile_" + props.country + ".png"} alt={props.country} /></li>
         <li>{props.country}</li>
         <li>{props.gold}</li>
         <li>{props.silver}</li>
